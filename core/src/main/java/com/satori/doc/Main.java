0dd05614339055d09048a18a0e574c1a0f37d6d0
yanfuyou <yanfuyou@iuctrip.com>
2024-05-28 21:05:33 +0800

    init

---
+++ core/src/main/java/com/satori/doc/Main.java
@@ -0,0 +1,11 @@
+package com.satori.doc;
+
+/**
+ * @author yfy
+ * @date 2024/05/28 20:26
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ core/src/test/java/SimpleTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yfy
+ * @date 2024/05/28 20:45
+ */
+public class SimpleTest {
+
+
+    @Test
+    public void genTest(){
+        
+    }
+
+}
